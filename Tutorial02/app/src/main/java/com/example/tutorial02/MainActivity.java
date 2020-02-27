89345ecb7d344387a8cb448270f20a9f46214705
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-02-27 12:49:20 +0000

    Tutorial 3 added write functionality in the app
    For now is customised to only write "Forza Lazio"

--- Tutorial02/app/src/main/java/com/example/tutorial02/MainActivity.java
+++ Tutorial02/app/src/main/java/com/example/tutorial02/MainActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         TextView textview = findViewById(R.id.helloworld);
 
         super.onNewIntent(intent);
+
+        if(intent.hasExtra(NfcAdapter.EXTRA_TAG)){
+            Toast.makeText(this,"Nfc extra tag intent", Toast.LENGTH_SHORT).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefMessage ndefMessage = createNdefMessage("Forza Lazio!");
+            writeNdefMessage(tag, ndefMessage);
+        }
+
     }
 
     @Override
@@ -56,6 +76,104 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         nfcAdapter.disableForegroundDispatch(this);
         super.onPause();
+    }
+
+    private void enableForegroundDispatchSystem(){
+        Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 ,intent, 0);
+
+        IntentFilter[]intentFilters = new IntentFilter[]{};
+        nfcAdapter.enableForegroundDispatch(this,pendingIntent, intentFilters, null);
+
+    }
+
+    private void disableForegroundDispatchSystem(){
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void formatTag(Tag tag, NdefMessage ndefMessage){
+        try{
+
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+
+            if (ndefFormatable==null){
+                Toast.makeText(this,"Tag is not NDEF formatable", Toast.LENGTH_SHORT).show();
+            }
+
+            ndefFormatable.connect();
+            ndefFormatable.format(ndefMessage);
+            ndefFormatable.close();
+
+            Toast.makeText(this, "Tag write finished", Toast.LENGTH_SHORT).show();
+
+        }catch(Exception e){
+            Log.e("formatTag", e.getMessage());
+        }
+    }
+
+    private void writeNdefMessage(Tag tag, NdefMessage ndefMessage){
+        try {
+
+            if (tag ==null){
+                Toast.makeText(this,"Tag object is null", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Ndef ndef = Ndef.get(tag);
+
+            if (ndef == null){
+                // format tag with the ndef format and writes the mssage
+                formatTag(tag,ndefMessage);
+            }else{
+                ndef.connect();
+
+                if (!ndef.isWritable()){
+                    Toast.makeText(this, "Tag is not writable...", Toast.LENGTH_SHORT).show();
+                    ndef.close();
+                    return;
+                }
+
+                ndef.writeNdefMessage(ndefMessage);
+                ndef.close();
+
+                Toast.makeText(this,"Tag written successfully", Toast.LENGTH_LONG).show();
+
+            }
+
+
+        }catch (Exception e){
+            Log.e("writeNdefMessage", e.getMessage());
+        }
+    }
+
+    private NdefRecord createTextRecord(String content) {
+        try {
+            byte[] language;
+            language = Locale.getDefault().getLanguage().getBytes("UTF-8");
+
+            final byte[] text = content.getBytes("UTF-8");
+            final int languageSize = language.length;
+            final int textLength = text.length;
+            final ByteArrayOutputStream payload = new ByteArrayOutputStream(1 + languageSize + textLength);
+
+            payload.write((byte) (languageSize & 0x1F));
+            payload.write(language, 0, languageSize);
+            payload.write(text, 0, textLength);
+
+            return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0], payload.toByteArray());
+
+        } catch (Exception e) {
+            Log.e("CreateTextRecord", e.getMessage());
+        }
+        return null;
+    }
+
+    private NdefMessage createNdefMessage (String content){
+        NdefRecord ndefRecord = createTextRecord(content);
+        NdefMessage ndefMessage = new NdefMessage(new NdefRecord[] { ndefRecord } );
+
+        return ndefMessage;
     }
 
 }
