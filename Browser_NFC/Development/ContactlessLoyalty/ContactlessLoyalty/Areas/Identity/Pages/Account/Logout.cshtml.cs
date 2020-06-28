7ceb00e8fc9a30f43e0f2d642093426d0edcda42
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-06-28 22:09:21 +0100

    Update logout partial

--- Browser_NFC/Development/ContactlessLoyalty/ContactlessLoyalty/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Browser_NFC/Development/ContactlessLoyalty/ContactlessLoyalty/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -37,7 +37,7 @@ namespace ContactlessLoyalty.Areas.Identity.Pages.Account
             }
             else
             {
-                return Page();
+                return RedirectToPage();
             }
         }
     }
