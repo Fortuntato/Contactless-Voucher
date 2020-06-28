d7849633f9825da02b0473717af4a801d9bc5153
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-06-28 14:47:57 +0100

    Scaffolding identity result

---
+++ Browser_NFC/Development/ContactlessLoyalty/ContactlessLoyalty/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ContactlessLoyalty.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ContactlessLoyalty.Areas.Identity.IdentityHostingStartup))]
+namespace ContactlessLoyalty.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Browser_NFC/Development/ContactlessLoyalty/ContactlessLoyalty/Startup.cs
+++ Browser_NFC/Development/ContactlessLoyalty/ContactlessLoyalty/Startup.cs
@@ -41,6 +41,7 @@ namespace ContactlessLoyalty
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
