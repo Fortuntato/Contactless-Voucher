07eea4662d243ec5e73dba0bd922b8f8887bd01f
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-07-02 23:17:44 +0100

    Update Model for User

--- Browser_NFC/Development/ContactlessLoyaltyWebApp/Controllers/HomeController.cs
+++ Browser_NFC/Development/ContactlessLoyaltyWebApp/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ namespace ContactlessLoyaltyWebApp.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Create", "User");
         }
 
         public IActionResult Privacy()
--- Browser_NFC/Development/ContactlessLoyaltyWebApp/Models/UserModel.cs
+++ Browser_NFC/Development/ContactlessLoyaltyWebApp/Models/UserModel.cs
@@ -14,15 +14,26 @@ namespace ContactlessLoyaltyWebApp.Models
 
         public int CardID { get; set; }
 
+        [DataType(DataType.Text)]
         [Required(ErrorMessage = "Please enter a name")]
         public string Name{ get; set; }
 
+        [Display(Name = "Mobile Phone")]
+        [DataType(DataType.PhoneNumber)]
         [Required(ErrorMessage = "Please enter a valid Mobile Phone")]
         public string MobilePhone { get; set; }
         
+        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Please enter a password")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "You must enter a longer password")]
         public string Password { get; set; }
 
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Your password and confirm password do not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "User Role")]
         public string UserRole { get; set; }
 
     }
