4e74decbe85bfecf0abf1e12f7ce9ee70c55353d
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-07-02 18:41:06 +0100

    Created new solution

---
+++ Browser_NFC/Development/ContactlessLoyaltyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactlessLoyaltyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
