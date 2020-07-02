93a6153ae50a2197144339355d3b15245fba4ed4
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-07-02 22:25:12 +0100

    Added migration files and database created

---
+++ Browser_NFC/Development/ContactlessLoyaltyWebApp/Migrations/20200702212428_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactlessLoyaltyWebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardID = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    MobilePhone = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    UserRole = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
