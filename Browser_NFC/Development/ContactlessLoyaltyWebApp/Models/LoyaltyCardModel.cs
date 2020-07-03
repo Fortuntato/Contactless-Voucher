25ccd8d76a0591be2c005f05669ec3c701bd6bde
Fortuntato <37523381+Fortuntato@users.noreply.github.com>
2020-07-03 01:08:19 +0100

    Integrate loyaltycard model

---
+++ Browser_NFC/Development/ContactlessLoyaltyWebApp/Models/LoyaltyCardModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactlessLoyaltyWebApp.Models
+{
+    public class LoyaltyCardModel
+    {
+        public int ID { get; set; }
+
+        public DateTime LastStampReceived { get; set; }
+        public int VoucherReceived{ get; set; }
+
+        public int StampsCollected { get; set; }
+
+        public List<UserModel> User { get; set; }
+    }
+}
--- Browser_NFC/Development/ContactlessLoyaltyWebApp/Models/UserModel.cs
+++ Browser_NFC/Development/ContactlessLoyaltyWebApp/Models/UserModel.cs
@@ -12,29 +12,31 @@ namespace ContactlessLoyaltyWebApp.Models
         
         public int ID { get; set; }
 
-        public int CardID { get; set; }
+        public List<LoyaltyCardModel> LoyaltyCard { get; set; }
 
+        [MaxLength(20)]
         [DataType(DataType.Text)]
         [Required(ErrorMessage = "Please enter a name")]
         public string Name{ get; set; }
 
+        [MaxLength(15)]
         [Display(Name = "Mobile Phone")]
         [DataType(DataType.PhoneNumber)]
         [Required(ErrorMessage = "Please enter a valid Mobile Phone")]
         public string MobilePhone { get; set; }
-        
+
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "Please enter a password")]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "You must enter a longer password")]
         public string Password { get; set; }
 
-        [Display(Name = "Confirm Password")]
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Your password and confirm password do not match")]
-        public string ConfirmPassword { get; set; }
+        //[Display(Name = "Confirm Password")]
+        //[DataType(DataType.Password)]
+        //[Compare("Password", ErrorMessage = "Your password and confirm password do not match")]
+        //public string ConfirmPassword { get; set; }
 
-        [Display(Name = "User Role")]
-        public string UserRole { get; set; }
+        //[Display(Name = "User Role")]
+        //public string UserRole { get; set; }
 
     }
 }
